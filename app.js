a82527d6e4e5461c1bbc181fccb73a768aaf4c5a
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-07-16 14:26:31 +0100

    wip

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 'use strict'
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra')
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    const response =  await page.goto('https://www.fiverr.com/');
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
-    console.log(response)
-    await page.screenshot({path: 'example.png'});
+// puppeteer usage as normal
+puppeteer.launch({ headless: true }).then(async browser => {
+    console.log('Running tests..')
+    const page = await browser.newPage()
+    let response = await page.goto('https://www.fiverr.com/')
 
-    await browser.close();
-})();
+    await page.waitForTimeout(5000)
+    await page.screenshot({ path: 'testresult1.png', fullPage: true })
+    await page.mouse.click(800, 630, {delay: 5000});
+
+    // const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+
+
+    // const press = await page.$x("//h2[contains(text(),'below')]");
+    // if (press) {
+    //     await press.click();
+    // }
+
+
+
+    // console.log(response)
+
+
+    await page.screenshot({ path: 'testresult2.png', fullPage: true })
+    await browser.close()
+    console.log(`All done, check the screenshot. ✨`)
+})
