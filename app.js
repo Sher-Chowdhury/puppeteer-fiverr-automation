cf4cdf41d50999e480f98a3b4dbeec30bd345f24
Sher Chowdhury <sher.chowdhury@ibm.com>
2022-07-16 14:46:27 +0100

    5 second hold is working

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const puppeteer = require('puppeteer-extra')
+const {installMouseHelper} = require('./install-mouse-helper');
+
 
 // add stealth plugin and use defaults (all evasion techniques)
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
@@ -10,13 +12,26 @@ puppeteer.use(StealthPlugin())
 puppeteer.launch({ headless: true }).then(async browser => {
     console.log('Running tests..')
     const page = await browser.newPage()
+
+    await installMouseHelper(page);
+
+
+
     let response = await page.goto('https://www.fiverr.com/')
+
+
 
     await page.waitForTimeout(5000)
     await page.screenshot({ path: 'testresult1.png', fullPage: true })
-    await page.mouse.click(800, 630, {delay: 5000});
+    // await page.mouse.move(135, 173);
+    // await page.mouse.down();
+    // await page.mouse.move(400, 330);
+    await page.mouse.click(400, 330, {delay: 5000});
 
     // const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+    await page.waitForTimeout(5000)
+
+
 
 
     // const press = await page.$x("//h2[contains(text(),'below')]");
